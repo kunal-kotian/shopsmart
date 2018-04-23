98009845df238c62a3d4e48c2570d0c5e90914d1
Kunal Kotian <k.a.kotian@gmail.com>
2018-04-22 23:03:40 -0700

    added code to read and write pickled files directly from S3

--- Notebooks/S3_read_write.py
+++ Notebooks/S3_read_write.py
@@ -5,31 +5,38 @@ from pyarrow.feather import write_feather, read_feather
 import s3fs
 
 
-def load_df_s3(bucket_name, filepath, filetype, sep=','):
+def load_df_s3(bucket_name, filepath, filetype='feather', sep=','):
     """Load a text/feather file located at on S3 and return it in a dataframe
 
     Parameters:
         bucket_name: name of the S3 bucket
         filepath: path of the file on S3 (relative to the S3 bucket's home dir)
-        filetype: one of 'text' or 'feather'
-        sep: delimiter; applicable to reading text files
+        filetype: one of 'text', 'feather', or 'pickle'
+        sep: delimiter; applicable to reading text files, otherwise ignored
     Example: 
-    df = load_df_s3(bucket_name='bucketymcbucket', filepath='data/topsecret', 
+    df = load_df_s3(bucket_name='bucketymcbucket', filepath='data/topsecret.pkl', 
                     filetype='text', sep='|')
     """
     if filetype == 'text':
         filepath = 's3://{}/{}'.format(bucket_name, filepath)
-        return pd.read_csv(filepath, sep=sep)
+        df = pd.read_csv(filepath, sep=sep)
 
     elif filetype == 'feather':
-    	# assuming the file is a feather file, if not a csv
-	    fs = s3fs.S3FileSystem()
-	    with fs.open('s3://{}/{}'.format(bucket_name, filepath)) as f:
-	        return read_feather(f)
+        fs = s3fs.S3FileSystem()
+        with fs.open('s3://{}/{}'.format(bucket_name, filepath)) as f:
+            df = read_feather(f)
+
+    elif filetype == 'pickle':
+        s3 = boto3.client('s3')
+        obj = s3.get_object(Bucket=bucket_name, Key=filepath)
+        df = pd.read_pickle(BytesIO(obj['Body'].read()))
+
+    return df
 
 
-def save_df_s3(df, bucket_name, filepath):
-    """ Save df to filepath on the S3 bucket in the feather format
+def save_df_s3(df, bucket_name, filepath, filetype='feather'):
+    """ Save df to filepath on the S3 bucket in the specified format.
+    Supported formats: feather, pickle.
 
     Parameters:
         df: pandas dataframe to be saved
@@ -42,5 +49,10 @@ def save_df_s3(df, bucket_name, filepath):
     """
     s3_resource = boto3.resource('s3')
     with BytesIO() as f:
-        write_feather(df, f)
-        s3_resource.Object(bucket_name, filepath).put(Body=f.getvalue())
+        if filetype == 'feather':
+            write_feather(df, f)
+            s3_resource.Object(bucket_name, filepath).put(Body=f.getvalue())
+
+        elif filetype == 'pickle':
+            pickle.dump(df, f)
+            s3_resource.Object(bucket_name, filepath).put(Body=f.getvalue())
